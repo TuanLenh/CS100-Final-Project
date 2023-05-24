109f11e0cf6c3c5e88b077641766546726dfc704
TuanLenh <tuanlenh3085@gmail.com>
2023-05-24 13:08:31 -0700

    initial test

---
+++ UnitTest/ReturnStatTest.cpp
@@ -0,0 +1,8 @@
+#include "../ReturnStat.h"
+#include "gtest/gtest.h"
+
+TEST(ReturnStatFunc, testHealth){
+    Character Mario(1, "Mario", 0, 0, 0, 0, "Normal");
+    Character Dummy(2, "Dummy", 0, 0, 0, 0, "Normal");
+    EXPECT_EQ (Mario.getAttack(), 20);
+}
