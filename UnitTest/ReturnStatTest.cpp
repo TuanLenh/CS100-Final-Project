a6dcb6e3d5c0529690d0d652c394b20ec0557b96
TuanLenh <tuanlenh3085@gmail.com>
2023-05-24 15:32:21 -0700

    test

--- UnitTest/ReturnStatTest.cpp
+++ UnitTest/ReturnStatTest.cpp
@@ -4,5 +4,6 @@
 TEST(ReturnStatFunc, testHealth){
     Character Mario(1, "Mario", 0, 0, 0, 0, "Normal");
     Character Dummy(2, "Dummy", 0, 0, 0, 0, "Normal");
+    // ReturnStat resetFunction(Mario, Dummy);
     EXPECT_EQ (Mario.getAttack(), 20);
 }
