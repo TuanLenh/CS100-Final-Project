d69e201eac3819e8bee65911c4eae986513a44c8
TuanLenh <tuanlenh3085@gmail.com>
2023-05-24 19:08:01 -0700

    completed unit test

--- UnitTest/ReturnStatTest.cpp
+++ UnitTest/ReturnStatTest.cpp
@@ -4,6 +4,33 @@
 TEST(ReturnStatFunc, testHealth){
     Character Mario(1, "Mario", 0, 0, 0, 0, "Normal");
     Character Dummy(2, "Dummy", 0, 0, 0, 0, "Normal");
-    // ReturnStat resetFunction(Mario, Dummy);
+    ReturnStat resetFunction;
+    resetFunction.returnToOriginal(Mario, Dummy);
+    EXPECT_EQ (Mario.getHealth(), 100);
+}
+
+TEST(ReturnStatFunc, testAttack){
+    Character Mario(1, "Mario", 0, 0, 0, 0, "Normal");
+    Character Dummy(2, "Dummy", 0, 0, 0, 0, "Normal");
+    ReturnStat resetFunction;
+    resetFunction.returnToOriginal(Mario, Dummy);
     EXPECT_EQ (Mario.getAttack(), 20);
 }
+
+TEST(ReturnStatFunc, testDefense){
+    Character Mario(1, "Mario", 0, 0, 0, 0, "Normal");
+    Character Dummy(2, "Dummy", 0, 0, 0, 0, "Normal");
+    ReturnStat resetFunction;
+    resetFunction.returnToOriginal(Mario, Dummy);
+    EXPECT_EQ (Mario.getDefense(), 16);
+}
+
+TEST(ReturnStatFunc, testStatus){
+    Character Mario(1, "Mario", 0, 0, 0, 0, "Frozen");
+    Character Dummy(2, "Dummy", 0, 0, 0, 0, "Normal");
+    ReturnStat resetFunction;
+    resetFunction.returnToOriginal(Mario, Dummy);
+    EXPECT_EQ (Mario.getStatus(), "Normal");
+}
+
+
