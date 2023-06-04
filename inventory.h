17120eeb799a568daf5153d18cc9a05eaccb9b74
Ashwin987 <ashwin.ramases@gmail.com>
2023-06-03 21:32:20 -0700

    updated inventory class

---
+++ inventory.h
@@ -0,0 +1,13 @@
+#ifndef INVENTORY
+#define INVENTORY
+#include <string>
+#include "Character.h"
+
+using namespace std;
+
+class Inventory{
+ 	public:
+        void useInventory(Character &char1, Character &char2);
+};
+
+#endif
